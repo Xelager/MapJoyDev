6b7ca3774489a7c8a4ad18174690821d5bf9d8c6
Alexsander Gazukin <77701153+Xelager@users.noreply.github.com>
2021-12-13 05:00:00 +0300

    Add api

---
+++ Map/app/src/main/java/com/joydev/map/services/LocationService.java
@@ -0,0 +1,171 @@
+package com.joydev.map.services;
+
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Looper;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.joydev.map.R;
+import com.joydev.map.UserClient;
+import com.joydev.map.models.User;
+import com.joydev.map.models.UserLocation;
+
+
+public class LocationService extends Service {
+
+    private static final String TAG = "LocationService";
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private final static long UPDATE_INTERVAL = 4 * 1000;  /* 4 secs */
+    private final static long FASTEST_INTERVAL = 2000; /* 2 sec */
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        startForeground(1, getNotification());
+    }
+
+    public Notification getNotification() {
+        String channel;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            channel = createChannel();
+        else {
+            channel = "";
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channel)
+                .setSmallIcon(android.R.drawable.ic_menu_mylocation)
+                .setContentTitle("")
+                .setContentText("");
+        Notification notification = mBuilder
+                .setPriority(Notification.PRIORITY_LOW)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+
+
+        return notification;
+    }
+
+    @NonNull
+    @TargetApi(26)
+    private synchronized String createChannel() {
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String name = "snap map fake location ";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+
+        NotificationChannel mChannel = new NotificationChannel("snap map channel", name, importance);
+
+        mChannel.enableLights(true);
+        mChannel.setLightColor(Color.BLUE);
+        if (mNotificationManager != null) {
+            mNotificationManager.createNotificationChannel(mChannel);
+        } else {
+            stopSelf();
+        }
+        return "snap map channel";
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: called.");
+        getLocation();
+        return START_NOT_STICKY;
+    }
+
+    private void getLocation() {
+
+        // ---------------------------------- LocationRequest ------------------------------------
+        // Create the location request to start receiving updates
+        LocationRequest mLocationRequestHighAccuracy = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "getLocation: stopping the location service.");
+            stopSelf();
+            return;
+        }
+        Log.d(TAG, "getLocation: getting location information.");
+        mFusedLocationClient.requestLocationUpdates(mLocationRequestHighAccuracy, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+
+                        Log.d(TAG, "onLocationResult: got location result.");
+
+                        Location location = locationResult.getLastLocation();
+
+                        if (location != null) {
+                            User user = ((UserClient)(getApplicationContext())).getUser();
+                            GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+                            UserLocation userLocation = new UserLocation(user, geoPoint, null);
+                            saveUserLocation(userLocation);
+                        }
+                    }
+                },
+                Looper.myLooper()); // Looper.myLooper tells this to repeat forever until thread is destroyed
+    }
+
+    private void saveUserLocation(final UserLocation userLocation){
+
+        try{
+            DocumentReference locationRef = FirebaseFirestore.getInstance()
+                    .collection(getString(R.string.collection_user_locations))
+                    .document(FirebaseAuth.getInstance().getUid());
+
+            locationRef.set(userLocation).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Log.d(TAG, "onComplete: \ninserted user location into database." +
+                                "\n latitude: " + userLocation.getGeo_point().getLatitude() +
+                                "\n longitude: " + userLocation.getGeo_point().getLongitude());
+                    }
+                }
+            });
+        }catch (NullPointerException e){
+            Log.e(TAG, "saveUserLocation: User instance is null, stopping location service.");
+            Log.e(TAG, "saveUserLocation: NullPointerException: "  + e.getMessage() );
+            stopSelf();
+        }
+
+    }
+}
---
+++ Map/app/src/main/java/com/joydev/map/ui/IProfile.java
@@ -0,0 +1,6 @@
+package com.joydev.map.ui;
+
+public interface IProfile {
+
+    void onImageSelected(int resource);
+}
